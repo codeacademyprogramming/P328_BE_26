6fff234179d89414eb1ec51efee44bda2c1735f5
Elchin Aghayev <Elchin@code.local>
2023-06-01 21:47:29 +0400

    project added

---
+++ P328Pustok/P328Pustok/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P328Pustok.Areas.Manage.Controllers
+{
+    [Authorize(Roles = "SuperAdmin,Admin")]
+    [Area("manage")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ P328Pustok/P328Pustok/wwwroot/js/main.js
@@ -0,0 +1,52 @@
+﻿$(document).on("click", ".modal-btn", function (e) {
+    e.preventDefault();
+
+    let url = $(this).attr("href");
+
+    fetch(url)
+        .then(response => {
+
+            console.log(response)
+            if (response.ok) {
+                return response.text()
+            }
+            else {
+                alert("Bilinmedik bir xeta bas verdi!")
+            }
+        })
+        .then(data => {
+            //$("#quickModal .product-title").text(data.book.name)
+            $("#quickModal .modal-dialog").html(data)
+            $("#quickModal").modal('show');
+        })
+})
+$(document).on("click", ".addtobasket", function (e) {
+    e.preventDefault();
+    let url = $(this).attr("href");
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                alert("xeta bas verdi")
+            }
+            return response.text()
+        })
+        .then(data => {
+            $(".cart-block").html(data)
+        })
+})
+
+$(document).on("click", ".removefrombasket", function (e) {
+    e.preventDefault();
+    let url = $(this).attr("href");
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                alert("xeta bas verdi")
+                return
+            }
+            return response.text()
+        })
+        .then(data => {
+            $(".cart-block").html(data)
+        })
+})
